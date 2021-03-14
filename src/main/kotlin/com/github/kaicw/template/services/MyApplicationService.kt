99458a88df2c36177ee5da511388becf11dd9d9d
GitHub Action <action@github.com>
2021-03-14 06:12:01 +0000

    Template cleanup

--- src/main/kotlin/com/github/kaicw/template/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/kaicw/template/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.kaicw.template.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.kaicw.template.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/kaicw/template/services/MyProjectService.kt
+++ src/main/kotlin/com/github/kaicw/template/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.kaicw.template.services
 
+import com.github.kaicw.template.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
